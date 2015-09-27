2645480ae517608711a987e2025f63334030ab16
rosiana <rosilutfi@gmail.com>
2015-09-28 04:24:18 +0700

    bisa identify infinite solution, belum assign persamaan

--- SPLSolver.java
+++ SPLSolver.java
@@ -6,9 +6,11 @@ class SPLSolver
 {
     double[][] A; //matriks
     double[] R; //hasil
+    String[] e; //persamaan
     int n; //baris
     int m; //kolom
-    boolean noSolution = false;
+    boolean noSolution = false; //true jika nggak ada solusi
+    boolean infiniteSolution = false; //true jika solusi tdk terbatas
 
     BufferedReader in;
     PrintWriter out;
@@ -103,19 +105,33 @@ class SPLSolver
     void AssignResult1()
     {
         noSolution = IsNoSolution();
+        infiniteSolution = IsInfiniteSolution();
         if (noSolution)
         {
             System.out.println("Tidak ada solusi");
         }
         else
         {
-            int n = A.length - 1;
-            int m = A[0].length - 1;
-            R = new double[n+1];
-            for(int i = 1; i <= n; i++)
+            if (infiniteSolution)
             {
-                R[i] = A[i][m];
-                System.out.println("x" + i + " = " + A[i][m]);
+                System.out.println("Solusi tidak terbatas");
+                GetInfiniteEquation();
+                int q = e.length - 1;
+                for (int p = 1; p <= q; p++)
+                {
+                    System.out.println(e[p]);
+                }
+            }
+            else
+            {
+                int n = A.length - 1;
+                int m = A[0].length - 1;
+                R = new double[n+1];
+                for(int i = 1; i <= n; i++)
+                {
+                    R[i] = A[i][m];
+                    System.out.println("x" + i + " = " + A[i][m]);
+                }
             }
         }
     }
@@ -123,20 +139,34 @@ class SPLSolver
     void AssignResult2()
     {
         noSolution = IsNoSolution();
+        infiniteSolution = IsInfiniteSolution();
         if (noSolution)
         {
             out.println("Tidak ada solusi");
         }
         else
         {
-            int n = A.length - 1;
-            int m = A[0].length - 1;
-            R = new double[n+1];
-            for(int i = 1; i <= n; i++)
+            if (infiniteSolution)
             {
-                R[i] = A[i][m];
-                out.print("x" + i + " = " + A[i][m]);
-                out.println();
+                out.println("Solusi tidak terbatas");
+                GetInfiniteEquation();
+                int q = e.length - 1;
+                for (int p = 1; p <= q; p++)
+                {
+                    out.println(e[p]);
+                }
+            }
+            else
+            {
+                int n = A.length - 1;
+                int m = A[0].length - 1;
+                R = new double[n+1];
+                for(int i = 1; i <= n; i++)
+                {
+                    R[i] = A[i][m];
+                    out.print("x" + i + " = " + A[i][m]);
+                    out.println();
+                }
             }
         }
         out.close();
@@ -264,7 +294,7 @@ class SPLSolver
         boolean b = true;
         int n = A.length - 1;
         int m = A[0].length - 2;
-        for (int p = n; p >= 1; p--) //tinjau dari bawah karena bawah duluan yang kosong
+        for (int p = n; p >= 1; p--) //tinjau dari baris bawah karena bawah duluan yang kosong
         {
             for (int q = 1; q <= m; q++)
             {
@@ -278,6 +308,90 @@ class SPLSolver
             }
         }
         return b;
+    }
+
+    boolean IsInfiniteColumn(int i)
+    {
+        boolean b = true;
+        int n = A.length - 1;
+        boolean one = false; //true jika ketemu angka 1
+        int zero = n-1; //counter angka 0
+        for (int p = 1; p <= n; p++)
+        {
+            if (A[p][i] == 0)
+            {
+                zero--;
+            }
+            else
+            {
+                if (A[p][i] == 1)
+                {
+                    one = true;
+                }
+            }
+        }
+        if (zero == 0 && one)
+        {
+            b = false;
+        }
+        return b;
+    }
+
+    boolean IsInfiniteSolution()
+    {
+        boolean b = false;
+        int m = A[0].length - 2;
+        for (int p = m; p >= 1; p--) //tinjau dari kolom paling belakang
+        {
+            if (IsInfiniteColumn(p))
+            {
+                b = true;
+            }
+        }
+        return b;
+    }
+
+    void GetInfiniteEquation()
+    {
+        int c = 0;
+        int m = A[0].length - 2;
+        for (int p = m; p >= 1; p--) //tinjau dari kolom paling belakang
+        {
+            if (!IsInfiniteColumn(p))
+            {
+                c++;
+            }
+            System.out.printf(" " + c);
+        }
+        e = new String[c+1]; //penampung persamaan
+        int[] r = new int[c+1]; //buat nyimpen suatu persamaan ada di baris mana
+        for (int q = 1; q <= m; q++)
+        {
+            if (!IsInfiniteColumn(q))
+            {
+                e[q] = "x" + q + " =";
+                int k = 1;
+                while (k <= n && A[k][q] != 1)
+                {
+                    k++;
+                }
+                System.out.println(" " + k);
+                r[q] = k;
+            }
+        }
+        for (int s = 1; s <= e.length; s++)
+        {
+            if (s == r[s])
+            {
+                for (int t = 1; t <= m; t++)
+                {
+                    if ((A[s][t] != 0) && (A[s][t] != 1))
+                    {
+                        e[s] += " " + (-1)*A[s][t] + "x" + t + " + ";
+                    }
+                }
+            }
+        }
     }
 
     // kelompok menu
